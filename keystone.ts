fb95da2386c1597e715032fe946dce8cce8cfee9
PublicCodeRobin <86063078+PublicCodeRobin@users.noreply.github.com>
2021-11-29 09:44:19 +0100

    add backend files

---
+++ keystone.ts
@@ -0,0 +1,58 @@
+import { config, createSchema } from '@keystone-next/keystone/schema';
+import 'dotenv/config';
+import { User } from './schemas/User';
+import { createAuth } from '@keystone-next/auth';
+import { statelessSessions, withItemData } from '@keystone-next/keystone/session';
+const {
+  DATABSE_URL = 'mongodb+srv://sick:8beKpYfHUPeRl03O@clustorr.zmgxn.mongodb.net/sickfits?retryWrites=true&w=majority',
+  COOKIE_SECRET,
+  FRONTEND_URL,
+} = process.env;
+
+const sessionConfig = {
+  maxAge: 60 * 60 * 24 * 360,
+  secret: COOKIE_SECRET,
+};
+
+const  { withAuth } = createAuth({
+  listKey: 'User',
+  identityField: 'email',
+  initFirstItem: {
+    fields: [
+      'name',
+      'email',
+      'password',
+    ],
+  },
+  secretField: 'password',
+});
+
+export default config({
+  db: {
+    adapter: 'mongoose',
+    url: DATABSE_URL,
+  },
+  lists: createSchema({
+    // scheme goes here
+    User,
+  }),
+  server: {
+    cors: {
+      origin: FRONTEND_URL,
+    },
+  },
+  ui: {
+  // TODO: Show UI only for people that pass the test
+    isAccessAllowed: ({ session }) => {
+      console.log({ session });
+      return !!session?.data || true;
+    },
+  },
+  session: withItemData(
+    statelessSessions(sessionConfig),
+    {
+      User: `id`,
+    }
+  ),
+  // TODO: change this for roles
+});
