1f592f05cbb4ab7d9b9d41c5b25439901a02508e
PublicCodeRobin <86063078+PublicCodeRobin@users.noreply.github.com>
2021-11-30 12:45:36 +0100

    add lots of things

--- keystone.ts
+++ keystone.ts
@@ -5,11 +5,15 @@ import { createAuth } from '@keystone-next/auth';
 import { statelessSessions, withItemData } from '@keystone-next/keystone/session';
 import { Product } from './schemas/Product';
 import { ProductImage } from './schemas/ProductImage';
+import { insertSeedData } from './seed-data';
+
+const { argv } = process;
+
 const {
   DATABSE_URL = 'mongodb+srv://sick:8beKpYfHUPeRl03O@clustorr.zmgxn.mongodb.net/sickfits?retryWrites=true&w=majority',
   COOKIE_SECRET,
   FRONTEND_URL,
-} = process.env;
+} = process.env || {};
 
 const sessionConfig = {
   maxAge: 60 * 60 * 24 * 360,
@@ -34,6 +38,12 @@ export default withAuth(
     db: {
       adapter: 'mongoose',
       url: DATABSE_URL,
+      async onConnect(keystone) {
+        console.log('Connected', keystone);
+        if (argv.includes('--seed-data')) {
+          await insertSeedData(keystone);
+        }
+      },
     },
     lists: createSchema({
     // scheme goes here
