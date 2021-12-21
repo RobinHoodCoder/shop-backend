71c8b5224e8e1009c175c3d45c36a466b30dada2
PublicCodeRobin <86063078+PublicCodeRobin@users.noreply.github.com>
2021-12-21 16:18:21 +0100

    make checkout work

--- mutations/checkout.ts
+++ mutations/checkout.ts
@@ -4,12 +4,65 @@ type Arguments = {
   token: string,
 }
 
+const graphql = String.raw;
+
 const checkout = async (
   root: any,
   { token } : Arguments,
   context: KeystoneContext
 ) : Promise<OrderCreateInput> => {
+  const { session } = context;
 
+  if (!!session) {
+    try {
+      const { itemId } = session;
+      console.log('Destructured ITem', itemId);
+
+      const user = await context.lists.User.findOne({
+        where: { id: itemId },
+        resolveFields: graphql`
+          id
+          name
+          email
+          cart {
+            id,
+            quantity
+            product {
+                name
+                price
+                description
+                id
+                photo {
+                    id
+                    image {
+                        id
+                        publicUrlTransformed
+                    }
+                }
+            }
+          }
+      `,
+      });
+      console.log(!!user, 'User?');
+      console.dir(user, { depth: null });
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  /*
+   * 1. Make sure to be signed in.
+   * 1.1 if so.. Query it.
+   */
+
+
+  // 2. Calc total price fot their order.
+
+  // 3. Create the payment with the stripe lib
+
+  // 4. Convert cart items to order items,
+
+  // Create order to return to frontend.
 };
 
 export default checkout;
