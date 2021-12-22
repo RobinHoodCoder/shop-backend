fdedc6f39d5d1300939c4d2b136b0aa227b958e8
RobinHoodCoder <robinsteinbach@live.nl>
2021-12-22 14:26:03 +0100

    ok

--- mutations/addToCart.ts
+++ mutations/addToCart.ts
@@ -1,73 +1,49 @@
-import { KeystoneContext, SessionContext } from '@keystone-next/types';
-import { CartItemCreateInput } from  '../.keystone/schema-types';
+/* eslint-disable */
+import { KeystoneContext, SessionStore } from '@keystone-next/types';
+import { CartItem } from '../schemas/CartItem';
 import { Session } from '../types';
 
-const AddToCart = async (
+import { CartItemCreateInput } from '../.keystone/schema-types';
+
+async function addToCart(
   root: any,
-  { productId }: {productId:string},
+  { productId }: { productId: string },
   context: KeystoneContext
-) : Promise<CartItemCreateInput> => {
-  console.log('Adding to cart');
-  // 1. check user, check if signed in.
-  const session = await context.session as Session;
-
-  if (!session?.itemId) {
-    throw new Error('You must be signed in to add to cart');
+): Promise<CartItemCreateInput> {
+  console.log('ADDING TO CART!');
+  // 1. Query the current user see if they are signed in
+  const sesh = context.session as Session;
+  if (!sesh.itemId) {
+    throw new Error('You must be logged in to do this!');
   }
-
+  // 2. Query the current users cart
   const allCartItems = await context.lists.CartItem.findMany({
-    where: {
-      user: {
-        id: session.itemId,
-      },
-      product: {
-        id: productId,
-      },
-    },
-    resolveFields: 'id,quantity',
+    where: { user: { id: sesh.itemId }, product: { id: productId } },
+    resolveFields: 'id,quantity'
   });
 
   const [existingCartItem] = allCartItems;
-  console.log('existingCartItem!!!', existingCartItem);
-
-
-  if (!!existingCartItem) {
-    const { quantity, id } = existingCartItem;
-    console.log(`Already ${existingCartItem.quantity} in cart`);
+  if (existingCartItem) {
+    console.log(existingCartItem)
+    console.log(
+      `There are already ${existingCartItem.quantity}, increment by 1!`
+    );
+    // 3. See if the current item is in their cart
+    // 4. if itis, increment by 1
     return await context.lists.CartItem.updateOne({
-      id,
-      data: {
-        quantity: quantity + 1,
-      },
+      id: existingCartItem.id,
+      data: { quantity: existingCartItem.quantity + 1 },
+      resolveFields: false,
     });
   }
-
+  // 4. if it isnt, create a new cart item!
   return await context.lists.CartItem.createOne({
     data: {
-      product: {
-        connect: {
-          id: productId,
-        },
-      },
-      user: {
-        connect: {
-          id: session.itemId,
-        },
-      },
-      quantity: 1,
+      product: { connect: { id: productId }},
+      user: { connect: { id: sesh.itemId }},
     },
-  });
+    resolveFields: false,
+  })
+}
 
-
-  // 2. Query the current user's cart
-
-  /*
-   * 3. See if current item is present in cart.
-   *  1. If so, update quantity.
-   *  2. If not, add new item to cart.
-   */
-
-  // check if product is in stock
-};
-
-export default AddToCart;
+export default addToCart;
