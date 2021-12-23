f00e6f9dd0b148ff79c1007d432f4d964b7b8117
RobinHoodCoder <robinsteinbach@live.nl>
2021-12-23 12:35:37 +0100

    ok

--- schemas/Order.ts
+++ schemas/Order.ts
@@ -1,27 +1,23 @@
+import {
+  integer,
+  text,
+  relationship,
+  virtual,
+} from '@keystone-next/fields';
 import { list } from '@keystone-next/keystone/schema';
-import { integer, relationship, text, virtual } from '@keystone-next/fields';
+import formatMoney from '../lib/formatMoney';
 
 export const Order = list({
   fields: {
     label: virtual({
-    // TODO: check this. May need function instead
-      resolver: (item) => {
-        return `Cool ${item.total}`;
-      },
       graphQLReturnType: 'String',
+      resolver(item) {
+        return `${formatMoney(item.total)}`;
+      },
     }),
     total: integer(),
-    items: relationship(
-      {
-        ref: 'OrderItem.order',
-        many: true,
-      }
-    ),
-    user: relationship(
-      {
-        ref: 'User.orders',
-      }
-    ),
+    items: relationship({ ref: 'OrderItem.order', many: true }),
+    user: relationship({ ref: 'User.orders' }),
     charge: text(),
   },
 });
